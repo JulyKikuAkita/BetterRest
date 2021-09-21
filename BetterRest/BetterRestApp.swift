b9eaba4566c7905204ec3bb53dfc0e87fa5c9c23
JulyKikuAkita <b92701105@gmail.com>
2021-09-20 19:09:22 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ifang Lee on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
